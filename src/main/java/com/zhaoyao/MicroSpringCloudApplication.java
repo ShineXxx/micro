4abf9b354af99c32e2f467f84657a34bcbd88815
zhaohaijin <zhaohaijin@seassoon.com>
2021-03-25 10:20:38 +0800

    微服务初始化

---
+++ src/main/java/com/zhaoyao/MicroSpringCloudApplication.java
@@ -0,0 +1,14 @@
+package com.zhaoyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author zhaoyao
+ */
+@SpringCloudApplication
+public class MicroSpringCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSpringCloudApplication.class, args);
+    }
+}
